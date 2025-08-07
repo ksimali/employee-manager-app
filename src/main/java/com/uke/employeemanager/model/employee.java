4c119c533b8771825f2e45c377b3215acab45d65
ksimali <cassim.ali@outlook.fr>
2025-08-07 17:20:49 -0400

    Update @GenerateValue annotation : -Change the GenerationType from AUTO to IDENTITY

--- src/main/java/com/uke/employeemanager/model/employee.java
+++ src/main/java/com/uke/employeemanager/model/employee.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 public class employee implements Serializable {
     // attributes
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, updatable = false)
     private Long id;
     private String name;
