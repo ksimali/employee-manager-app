3b527ccd88531063fa44e5d7d8fb93a8c090fd19
ksimali <cassim.ali@outlook.fr>
2025-08-09 17:51:07 -0400

    Add some comment into EmployeeRepository Class

--- src/main/java/com/uke/employeemanager/repository/EmployeeRepository.java
+++ src/main/java/com/uke/employeemanager/repository/EmployeeRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    // Custom delete method - Spring Data JPA will implement it based on method name
     void deleteEmployeeById(Long id);
 
+    // Custom find method returning an Optional<Employee>
     Optional<Employee> findEmployeeById(Long id);
 }
