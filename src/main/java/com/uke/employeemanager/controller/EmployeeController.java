72addce07e285549f97fbda9c4f1c4903fe386e2
ksimali <cassim.ali@outlook.fr>
2025-08-12 21:26:00 -0400

    -Add data.json file & Add Cors configuration to EmployeeController

--- src/main/java/com/uke/employeemanager/controller/EmployeeController.java
+++ src/main/java/com/uke/employeemanager/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/employee")
 public class EmployeeController {
     private final EmployeeService employeeService;
